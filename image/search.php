e982c853d00326f346e895c24af2d347fabdd53b
amin007 <helmi007@yahoo.com>
2018-03-25 13:58:17 +0800

    Add files via upload

---
+++ image/search.php
@@ -0,0 +1,31 @@
+ <!DOCTYPE html>
+<html>
+<head>
+<title>my site</title>
+ <!-- Bootstrap Core CSS -->
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel='stylesheet' type='text/css' />
+<!-- Custom CSS -->
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"> 
+</head>
+<body>
+<hr>
+
+<!-- Place this tag where you want both of the search box and the search results to render -->
+<gcse:search></gcse:search><hr>
+
+<hr>
+
+<!-- jQuery -->
+<!-- script src="https://code.jquery.com/jquery-2.2.3.min.js"></script -->
+<!-- Put the following javascript before the closing  tag. -->
+<script>
+(function() {
+  var cx = '016724384925099384635:s9jmb6xrf-w'; // Insert your own Custom Search engine ID here
+  var gcse = document.createElement('script'); gcse.type = 'text/javascript'; gcse.async = true;
+  gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s);
+})();
+</script>
+
+</body>
+</html>
